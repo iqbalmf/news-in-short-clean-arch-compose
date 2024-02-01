b8db0e4293e38593755116a55898be0d5954fd5d
Iqbal Fauzan <iqbalmf68@gmail.com>
2024-02-01 13:33:32 +0700

    fixing bug index out of bound

--- app/src/main/java/net/iqbalfauzan/newsinshort/ui/screens/HomeScreen.kt
+++ app/src/main/java/net/iqbalfauzan/newsinshort/ui/screens/HomeScreen.kt
@@ -2,11 +2,10 @@ package net.iqbalfauzan.newsinshort.ui.screens
 
 import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -14,54 +13,56 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import net.iqbalfauzan.newsinshort.data.entity.NewsResponse
 import net.iqbalfauzan.newsinshort.ui.components.EmptyStateComponent
 import net.iqbalfauzan.newsinshort.ui.components.Loader
-import net.iqbalfauzan.newsinshort.ui.components.NewsList
 import net.iqbalfauzan.newsinshort.ui.components.NewsRowComponent
 import net.iqbalfauzan.newsinshort.ui.navigation.AppNavigationGraph
 import net.iqbalfauzan.newsinshort.ui.viewmodel.NewsViewModel
 import net.iqbalfauzan.utilities.ResourceState
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(
     newsViewModel: NewsViewModel = hiltViewModel()
 ) {
     val newsRes by newsViewModel.news.collectAsState()
 
-    val pagerState = rememberPagerState(
-        initialPage = 0,
-        initialPageOffsetFraction = 0f
-    ) {
-        100
+    when (newsRes) {
+        is ResourceState.Loading -> {
+            Loader()
+        }
+
+        is ResourceState.Success -> {
+            val response = (newsRes as ResourceState.Success).data
+            if (response.articles.isNotEmpty()) {
+                VerticalPagerWithIndicator(response = response)
+            } else {
+                EmptyStateComponent()
+            }
+        }
+
+        is ResourceState.Error -> {
+            val error = newsRes as ResourceState.Error
+            Log.e(HomeScreen().javaClass.simpleName.toString(), error.error)
+            EmptyStateComponent()
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun VerticalPagerWithIndicator(response: NewsResponse) {
+    val pagerState = rememberPagerState {
+        response.articles.size
     }
     VerticalPager(
         state = pagerState,
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxHeight(),
         pageSize = PageSize.Fill,
         pageSpacing = 8.dp
     ) { page: Int ->
-        when (newsRes) {
-            is ResourceState.Loading -> {
-                Loader()
-            }
-
-            is ResourceState.Success -> {
-                val response = (newsRes as ResourceState.Success).data
-                if (response.articles.isNotEmpty()) {
-                    NewsRowComponent(page, response.articles.get(page))
-                } else {
-                    EmptyStateComponent()
-                }
-            }
-
-            is ResourceState.Error -> {
-                val error = newsRes as ResourceState.Error
-                Log.e(HomeScreen().javaClass.simpleName.toString(), error.error)
-            }
-        }
+        NewsRowComponent(page, response.articles[page])
     }
-
 }
 
 @Preview
